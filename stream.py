5d756118130074ff6dd495dd184f7ec3a6e4ab40
kemfic <19ficici.kemal@gmail.com>
2018-12-13 18:04:45 -0500

    timer works

--- stream.py
+++ stream.py
@@ -156,6 +156,7 @@ if __name__ == '__main__':
 
 
   disp_view = MapViewer()
+  timer = []
   while(cap.isOpened()):
     start = time.time()
     ret, frame = cap.read()
@@ -172,7 +173,9 @@ if __name__ == '__main__':
     cv2.imshow('traj', stream.annotate_traj)
 
     stop = time.time()
-    print(1/(stop-start))
+    timer.append(1/(stop-start))
+
+    print(np.mean(timer))
     if cv2.waitKey(1) & 0xFF == ord('q'):
       print("exiting...")
       break
